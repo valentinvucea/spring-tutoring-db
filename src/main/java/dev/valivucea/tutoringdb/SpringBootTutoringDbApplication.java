77c5e321ddcb7abfb838580fe8b4cfebd76bbaab
Valentin Vucea <valentin@itworksdc.com>
2023-02-12 15:19:54 -0500

    First commit

---
+++ src/main/java/dev/valivucea/tutoringdb/SpringBootTutoringDbApplication.java
@@ -0,0 +1,13 @@
+package dev.valivucea.tutoringdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutoringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutoringDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/valivucea/tutoringdb/SpringBootTutoringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.valivucea.tutoringdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutoringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

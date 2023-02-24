76a748fc0b66ab58f3bfeaf5937395ce73d4eb82
Valentin Vucea <valentin@itworksdc.com>
2023-02-24 17:47:05 -0500

    Changes to GradesController - adjusted list page, created add page

--- src/main/java/dev/valivucea/tutoringdb/controller/GradesController.java
+++ src/main/java/dev/valivucea/tutoringdb/controller/GradesController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import dev.valivucea.tutoringdb.model.Grade;
 import dev.valivucea.tutoringdb.service.GradeService;
@@ -20,9 +22,21 @@ public class GradesController {
     public String showGradesList(Model model) {
         List<Grade> grades = gradeService.getGradeList();
         
-        model.addAttribute("title", "Spring Boot Tutorial | Grades List");
+        model.addAttribute("titleThis", "Spring Boot Tutorial | Grades List");
         model.addAttribute("grades", grades);
 
         return "/grades/index";
     }
+
+    @GetMapping({"grades/add"})
+    public String showAddGradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "/grades/add";
+    }
+    
+    @PostMapping({"grades/add"})
+    public String createNewGrade(@ModelAttribute Grade grade, Model model) {
+        gradeService.createGrade(grade);
+        return "redirect:/grades/index";
+    }     
 }

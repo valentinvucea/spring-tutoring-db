a8d569588999a31b0f267099ca8090160422b93f
Valentin Vucea <valentin@itworksdc.com>
2023-03-20 10:24:41 -0400

    Add the "This Quarter" aggregated data in the home page

--- src/main/java/dev/valivucea/tutoringdb/controller/HomeController.java
+++ src/main/java/dev/valivucea/tutoringdb/controller/HomeController.java
@@ -1,16 +1,40 @@
 package dev.valivucea.tutoringdb.controller;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import dev.valivucea.tutoringdb.service.SessionService;
+import dev.valivucea.tutoringdb.service.StudentSessionReport;
+
 @Controller
 public class HomeController {
     
+    @Autowired
+    private SessionService sessionService;
+    
     @GetMapping("/")
     public String showHomePage(Model model) {
-        model.addAttribute("message", "Hello World!");
+        // Find the current year and quarter
+        LocalDate today = LocalDate.now(); 
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int quarter = ((month - 1 ) / 3 ) + 1;
 
+        // Get the student data for the computed year and quarter
+        HashMap<Long, StudentSessionReport> students = sessionService.getDurationByStudentAndQuarter(year, quarter);
+
+        // Send the date to view
+        model.addAttribute("students", students);
+
+        // Return the view template path
         return "home/index";
     }
+
+
 }

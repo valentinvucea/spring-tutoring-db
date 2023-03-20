b4411b4a5f51928df1d6f8c86fbe9b56ee8294b1
Valentin Vucea <valentin@itworksdc.com>
2023-03-20 13:52:58 -0400

    Rework the javascript in the Reports page

--- src/main/resources/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Reload page on Reports page
-function reloadPage() {
-    // get year and quarter selected
-    const year = document.getElementById('year').value;
-    const quarter = document.getElementById('quarter').value;
-    
-    // redirect to the reports page with the new year and quarter in URL
-    location.href = '/reports/' + year + '/' + quarter;
-}
